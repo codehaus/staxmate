963f97fd0ddc15473839661371ceed7a90012174
cowtowncoder <cowtowncoder@c55428bd-0324-0410-a96c-c68cb27eb1d7>
2007-02-20 04:42:33 +0000

    Adding one more unit test, which demonstrates a bug (to be added to Jira, need to get Jira enabled first).

---
+++ src/test/staxmate/in/TestSimpleText.java
@@ -0,0 +1,36 @@
+package staxmate.in;
+
+import java.io.*;
+
+import javax.xml.stream.*;
+
+import org.codehaus.staxmate.SMInputFactory;
+import org.codehaus.staxmate.in.*;
+
+/**
+ *
+ * @author Michel Goldstein
+ * @author Tatu Saloranta
+ */
+public class TestSimpleText
+    extends BaseReaderTest
+{
+    public void testSimpleRead()
+        throws XMLStreamException
+    {
+        final String text = "1";
+        String XML = "<lvl1><lvl2>" + text + "</lvl2></lvl1>";
+        XMLStreamReader sr = XMLInputFactory.newInstance().createXMLStreamReader(new StringReader(XML));
+        SMInputCursor rootc = SMInputFactory.rootElementCursor(sr);
+        rootc.getNext();
+        String elemName = rootc.getLocalName();
+        assertEquals("lvl1",elemName);
+        SMInputCursor mainC = rootc.childElementCursor();
+        while(mainC.getNext() != null) {
+            SMInputCursor child = mainC.childCursor();
+            child.getNext();
+            String valText = child.getText();
+            assertEquals(text,valText);
+        }
+    }
+}
