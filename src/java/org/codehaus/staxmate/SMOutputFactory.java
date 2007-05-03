b6721b2eb8d953aa3aea754fc04736cf3bb0a997
cowtowncoder <cowtowncoder@c55428bd-0324-0410-a96c-c68cb27eb1d7>
2007-05-03 03:34:59 +0000

    Adding BSD license, references.

--- src/java/org/codehaus/staxmate/SMOutputFactory.java
+++ src/java/org/codehaus/staxmate/SMOutputFactory.java
@@ -1,3 +1,18 @@
+/* StaxMate XML processing library
+ *
+ * Copyright (c) 2005- Tatu Saloranta, tatu.saloranta@iki.fi
+ *
+ * Licensed under the License specified in the file LICENSE which is
+ * included with the source code.
+ * You may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.codehaus.staxmate;
 
 import javax.xml.stream.FactoryConfigurationError;
